75e95f77d61913965a6adbab33e21b85e1bcdedf
durve-greycube <zelam@greycube.in>
2025-01-30 13:12:28 +0530

    fix:business date for zomato/swiggy, price list from cost center

--- farki/farki/doctype/pet_pooja_log/pet_pooja_log.py
+++ farki/farki/doctype/pet_pooja_log/pet_pooja_log.py
@@ -8,6 +8,7 @@ from frappe.utils import getdate, cstr, add_to_date, get_time, flt, cint, get_li
 from frappe.model.document import Document
 from erpnext import get_company_currency, get_default_company
 from frappe.model.meta import get_field_precision
+from erpnext.stock.get_item_details import get_price_list_rate_for
 
 class PetPoojaSICreatinoError(frappe.ValidationError):
 	pass
@@ -38,6 +39,7 @@ def create_sales_invoice(docname):
 		default_float_precision = cint(frappe.db.get_default("float_precision")) or 2
 		print(default_currency_precision,"=====")
 
+		# create SI
 		sales_invoice_doc = frappe.new_doc("Sales Invoice")
 		sales_invoice_doc.cost_center = cost_center_doc.name
 		sales_invoice_doc.set_posting_time = 1
@@ -47,11 +49,8 @@ def create_sales_invoice(docname):
 		sales_invoice_doc.custom_pet_pooja_order_id = order_details.get('orderID')
 
 		zomato_customer = farki_settings_doc.default_zomato_customer
-		zomato_customer_price_list = frappe.db.get_value('Customer', zomato_customer, 'default_price_list')
 		swiggy_customer = farki_settings_doc.default_swiggy_customer
-		swiggy_customer_price_list = frappe.db.get_value('Customer', swiggy_customer, 'default_price_list')
-		# phone_pe = frappe.db.get_single_value('Farki Settings', 'phone_pe')
-		default_selling_price_list = frappe.db.get_single_value('Selling Settings', 'selling_price_list')
+
 		sales_taxes_and_charges = frappe.db.get_value('Sales Taxes and Charges Template',{"is_default":1}, 'name')
 		sales_taxes_and_charges_doc = frappe.get_doc('Sales Taxes and Charges Template', sales_taxes_and_charges)
 		payment_type = order_details.get('payment_type')
@@ -60,12 +59,32 @@ def create_sales_invoice(docname):
 		for row in sales_taxes_and_charges_doc.taxes:
 			sales_invoice_doc.append('taxes', row)
 		payments_row = sales_invoice_doc.append('payments', {})
+
+		mode_of_payment_found = False
+		pl_found = False
+		si_customer = None
+
+		# order from POS and fill Payments child table
 		if order_from == 'POS':
-			sales_invoice_doc.customer = cost_center_doc.custom_default_b2c_customer
-			sales_invoice_doc.selling_price_list = default_selling_price_list
+			si_customer = cost_center_doc.custom_default_b2c_customer
+			if len(cost_center_doc.custom_customer_wise_price_list)>0:
+				for pl in cost_center_doc.custom_customer_wise_price_list:
+					if pl.customer == cost_center_doc.custom_default_b2c_customer:
+						if pl.price_list:
+							pl_found = True
+							print(pl.price_list,"Price list")
+							sales_invoice_doc.selling_price_list = pl.price_list
+							break
+						else :	
+							frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(cost_center_doc.custom_default_b2c_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+					else :
+						pl_found = False
+				if pl_found == False:
+					frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(cost_center_doc.custom_default_b2c_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+			else :
+				frappe.throw(_("Please set customer and appropriate price list in Cost Center {0}".format(get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
 			sales_invoice_doc.is_pos = 1
-			mode_of_payment_found = False
-			# payments_row = sales_invoice_doc.append('payments', {})
+		
 			if payment_type != "Other":
 				if len(cost_center_doc.custom_pp_vs_erpnext_mode_of_payment_mapping) > 0:
 					for row in cost_center_doc.custom_pp_vs_erpnext_mode_of_payment_mapping:
@@ -98,8 +117,7 @@ def create_sales_invoice(docname):
 							mode_of_payment_found = False
 					if mode_of_payment_found == False:
 						frappe.throw(_("Please set appropriate Mode of Payment for Payment type {0} in Farki Settings {1}".format(frappe.bold(custom_payment_type),get_link_to_form("Farki Settings",farki_settings_doc.name))),exc=PetPoojaSICreatinoError)
-				# else :
-				# 	frappe.throw(_("Please set appropriate Payment type and Mode of Payment in Farki Settings {0}".format(get_link_to_form("Farki Settings",farki_settings_doc.name))),exc=PetPoojaSICreatinoError)
+				
 				elif len(cost_center_doc.custom_pp_vs_erpnext_mode_of_payment_mapping) > 0:
 					for row in cost_center_doc.custom_pp_vs_erpnext_mode_of_payment_mapping:
 						if row.pp_mode_of_payment == custom_payment_type:
@@ -114,31 +132,66 @@ def create_sales_invoice(docname):
 							mode_of_payment_found = False
 					if mode_of_payment_found == False:
 						frappe.throw(_("Please set appropriate Mode of Payment for Payment type {0} in Cost Center {1}".format(frappe.bold(frappe.bold(custom_payment_type)),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
-				else :
-					frappe.throw(_("Please set appropriate Payment type and Mode of Payment in Cost Center {0}".format(get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
 
+				else :
+					frappe.throw(_("Please set appropriate Payment type and Mode of Payment in Cost Center {0} or in Farki Settings {1}".format(get_link_to_form("Cost Center",cost_center_doc.name)),get_link_to_form("Farki Settings",farki_settings_doc.name)),exc=PetPoojaSICreatinoError)
+
+			# POS Business date logic
 			date_time_limit = frappe.db.get_value('Mode of Payment', mode_of_payment, 'custom_consider_in_previous_business_date_till')
-			# print(date_time_limit,"====date_time_limit",type(get_time(date_time_limit)))
 			if date_time_limit:
 				if create_date_time.time() < get_time(date_time_limit):
 					sales_invoice_doc.posting_date = add_to_date(create_date_time.date(), days=-1)
 					sales_invoice_doc.posting_time = get_time("23:59:59")
 					sales_invoice_doc.due_date = add_to_date(create_date_time.date(), days=-1)
+					sales_invoice_doc.custom_business_date = add_to_date(create_date_time.date(), days=-1)
+
 			payments_row.amount = sales_invoice_doc.rounded_total
 
+		# Zomato Swiggy price list Logic
 		elif order_from == 'Zomato':
-			sales_invoice_doc.customer = zomato_customer
-			if zomato_customer_price_list:
-				sales_invoice_doc.selling_price_list = zomato_customer_price_list
+			si_customer = zomato_customer
+			if len(cost_center_doc.custom_customer_wise_price_list)>0:
+				for pl in cost_center_doc.custom_customer_wise_price_list:
+					if pl.customer == zomato_customer:
+						if pl.price_list:
+							pl_found = True
+							sales_invoice_doc.selling_price_list = pl.price_list
+							break
+						else :	
+							frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(zomato_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+					else :
+						pl_found = False
+				if pl_found == False:
+					frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(zomato_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
 			else :
-				frappe.throw(_("Please set default price list for zomato customer in {0}".format(get_link_to_form("Customer",zomato_customer))),exc=PetPoojaSICreatinoError)
+				frappe.throw(_("Please set customer and appropriate price list in Cost Center {0}".format(get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+
 		elif order_from == 'Swiggy':
-			sales_invoice_doc.customer = swiggy_customer
-			if swiggy_customer_price_list:
-				sales_invoice_doc.selling_price_list = frappe.db.get_value('Customer', swiggy_customer, 'default_price_list')
+			si_customer = swiggy_customer
+			if len(cost_center_doc.custom_customer_wise_price_list)>0:
+				for pl in cost_center_doc.custom_customer_wise_price_list:
+					if pl.customer == swiggy_customer:
+						if pl.price_list:
+							pl_found = True
+							sales_invoice_doc.selling_price_list = pl.price_list
+							break
+						else :	
+							frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(swiggy_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+					else :
+						pl_found = False
+				if pl_found == False:
+					frappe.throw(_("Please price list for {0} customer in cost center {1}".format(frappe.bold(swiggy_customer),get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
 			else :
-				frappe.throw(_("Please set default price list for swiggy customer in {0}".format(get_link_to_form("Customer",swiggy_customer))),exc=PetPoojaSICreatinoError)
-		
+				frappe.throw(_("Please set customer and appropriate price list in Cost Center {0}".format(get_link_to_form("Cost Center",cost_center_doc.name))),exc=PetPoojaSICreatinoError)
+
+		# Zomato Swiggy Business Date
+		if order_from == 'Swiggy' or order_from == 'Zomato':
+			business_date_limit = cost_center_doc.custom_consider_in_previous_business_date_till
+			if business_date_limit:
+				if create_date_time.time() < get_time(business_date_limit):
+					sales_invoice_doc.custom_business_date = add_to_date(create_date_time.date(), days=-1)
+
+		sales_invoice_doc.customer = si_customer
 		sales_invoice_doc.custom_pp_mode_of_payment = custom_payment_type if payment_type=="Other" else payment_type
 		sales_invoice_doc.territory = cost_center_doc.custom_territory
 		sales_invoice_doc.update_stock = 1
@@ -146,6 +199,7 @@ def create_sales_invoice(docname):
 		company_defaut_income_account = frappe.db.get_value('Company', cost_center_doc.company, 'default_income_account')
 		place_of_supply = farki_settings_doc.place_of_supply
 
+		# fetch defult precision
 		company = get_default_company()
 		company_currency = erpnext.get_company_currency(company)
 		discount_precision=(
@@ -166,18 +220,28 @@ def create_sales_invoice(docname):
 		)
 		print(rate_precision,"-----")
 
+		# Fill Items child table
 		order_item_details = properties.get('OrderItem')
 		print(order_item_details,"=====---------------------",len(order_item_details),type(order_item_details))
 		for item in order_item_details:
 			item_rate_as_per_selling_price_list=0
+			item_code = item.get('sap_code')
 			item_row = sales_invoice_doc.append('items', {})
-			if item.get('sap_code') == None or item.get('sap_code') == "":
+			if item_code == None or item_code == "":
 				frappe.throw(_("sap code must have value"),exc=PetPoojaSICreatinoError)
 			else :
-				item_row.item_code = item.get('sap_code')
+				item_row.item_code = item_code
 			item_row.qty = item.get('quantity')
-			item_price = frappe.db.get_value('Item Price',{"item_code":item.get('sap_code'),"price_list":sales_invoice_doc.selling_price_list}, 'price_list_rate')
+			# get item price based on customer and price list
+			pl_args = frappe._dict({
+				'customer':si_customer,
+				'price_list':sales_invoice_doc.selling_price_list,
+				'qty':1,
+				'uom':frappe.db.get_value('Item', item_code, 'stock_uom')
+			})
+			item_price = get_price_list_rate_for(pl_args,item_code)
 			print(item_price,"====item_price",sales_invoice_doc.selling_price_list)
+			# Compliementary case
 			if order_details.get('status') == 'Complimentary':
 				item_row.rate = 0
 				item_row.discount_percentage = 100
@@ -196,7 +260,7 @@ def create_sales_invoice(docname):
 			item_row.warehouse = cost_center_doc.custom_warehouse
 			print(item_row.discount_amount,"amount")
 			item_row.income_account = company_defaut_income_account
-			item_tax_template = frappe.db.get_value('Item Tax',{"parent":item.get('sap_code')}, 'item_tax_template')
+			item_tax_template = frappe.db.get_value('Item Tax',{"parent":item_code}, 'item_tax_template')
 			item_row.item_tax_template = item_tax_template
 
 		sales_invoice_doc.place_of_supply = place_of_supply
@@ -205,7 +269,7 @@ def create_sales_invoice(docname):
 		print(type(cstr(order_details.get('orderID'))),"====order_details.get('orderID')")
 		print(type(rest_id),"====rest_id")
 		unique_id = _("{0}_{1}_{2}".format(order_details.get('created_on') ,cstr(order_details.get('orderID')) , rest_id))
-
+		# cancelled order case
 		if order_details.get('status') == 'Cancelled':
 			exists_si = frappe.db.get_all("Sales Invoice",
 									filters={"custom_pp_unique_order_id":unique_id},
@@ -217,7 +281,6 @@ def create_sales_invoice(docname):
 					exists_si_doc.cancel()
 					invoice_status = "Cancelled"
 					frappe.db.set_value("Pet Pooja Log",docname,"invoice_status",invoice_status)
-					# ppl_doc.save(ignore_permissions=True)
 					return
 				else:
 					frappe.throw(_("Sales Invoice {0} is not submitted".format(exists_si[0].name)),exc=PetPoojaSICreatinoError)
@@ -233,13 +296,11 @@ def create_sales_invoice(docname):
 		sales_invoice_doc.custom_pet_pooja_log_id = ppl_doc.name
 		sales_invoice_doc.custom_pet_pooja_order_date_time = create_date_time
 		sales_invoice_doc.custom_pp_unique_order_id = unique_id
-		# sales_invoice_doc.discount_amount = order_details.get('discount_total')
 
-		print(sales_invoice_doc.items[0].discount_amount,"==== before",sales_invoice_doc.items[0].rate,"rate")
 		sales_invoice_doc.run_method("set_missing_values")
 		sales_invoice_doc.run_method("set_other_charges")
 		sales_invoice_doc.run_method("calculate_taxes_and_totals")
-		print(sales_invoice_doc.items[0].discount_amount,"====",sales_invoice_doc.items[0].rate,"rate")
+
 		print(sales_invoice_doc.rounded_total,"====")
 		payments_row.amount = sales_invoice_doc.rounded_total
 		sales_invoice_doc.save(ignore_permissions=True)
